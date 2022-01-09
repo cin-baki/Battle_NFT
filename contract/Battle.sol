0d108e55049d50f657abfa7ba356e0b5458cda11
Dang Nguyen <43028613+cin-baki@users.noreply.github.com>
2022-01-09 22:10:56 +0700

    Add files via upload
    update deploy crab from game backend

--- contract/Battle.sol
+++ contract/Battle.sol
@@ -22,11 +22,11 @@ contract Battle is ReentrancyGuard {
 
 
 
-    /// @notice player deploy crab Player address -> Slot to deploy crab -> crab token ID 
-    mapping(address => mapping(uint256 => uint256))  public PlayerSlotTokenid;
+    /// @notice player deploy crab: BattleGround ID -> Player address -> Slot to deploy crab -> crab token ID 
+    mapping(uint256 => mapping(address => mapping(uint256 => uint256)))  public BattlePlayerSlotTokenid;
+    /// @notice add player to battle ground: BattleGround ID to Player
+    mapping(uint256 => address[2]) public BattleGround;
 
-    /// @notice Player in battle ground
-    address[0] payable public Player;
     address public sc_crab
 
 
@@ -50,33 +50,36 @@ contract Battle is ReentrancyGuard {
     constructor (address _sc_crab) public {
     }
 
-    /// @notice Method for adding player to battle ground
+    /// @notice Method for adding player to battle ground, call by system
+    /// @param _battleGroundID in game battle scence ID that want to add player
     /// @param _player Player address be added 
-    function Add_Player(address _player) external ValidPlayer ValidBattleSlot {
+    function Add_Player(uint256 _battleGroundID, address[2] payable _player) external ValidBattleSlot {
         /// body code
     }
 
-    /// @notice Method for deploy crab in battle ground
+    /// @notice Method for deploy crab in battle ground, call by system after add_player, this function also verify if player is added or not
+    /// @param _battleGroundID in game battle scence ID that player want deploy crab
     /// @param _player Player address that want deploy crab
     /// @param _slot_index slot that want to deploy crab
     /// @param _crabTokenID which crabID that want to deploy in selected slot
-    function deploy_crab(address _player, uint256 _slot_index, uint256 _crabTokenID) external ValidPlayerSlot {
+    function deploy_crab(uint256 _battleGroundID, address  _player, uint256 _slot_index, uint256 _crabTokenID) external ValidPlayer ValidPlayerSlot {
         /// body code
     }
 
-    /// @notice Method for player to confirm when they are ready
-    /// @param _player Player address who confirm ready
-    function Confirm(address _player) external returns (bool) {
-        /// body code
-    }
-
-    /// @notice Method to calculate player battle point base on their deplot crab
-    /// @param _player Player address who confirm ready
+    /// @notice Method to calculate player battle point base on their deployed crab, call in choose_winner function
+    /// @param _player Player address to calculate point
     function Calculate_Player_Point(address _player) internal returns (uint256) {
         ///body code
     }
 
-    /// @notice Method to choose winner base on their battle point
-    function Choose_Winner() external payable nonReentrant{
+    /// @notice Method to choose winner base on their battle point and transfer 100 shell to winner, call by system
+    /// @param _battleGroundID in game battle scence ID that want to choose winner
+    function Choose_Winner(uint256 _battleGroundID) external payable nonReentrant{
+        /// body code
+    }
+
+    /// @notice Method to free battle ground and crab slot after winner was choosen, call by system
+    /// @param _battleGroundID in game battle scence ID that want to free
+    function FreeBattleGround(uint256 _battleGroundID, address  _player, uint256 _slot_index) external{
         /// body code
     }
